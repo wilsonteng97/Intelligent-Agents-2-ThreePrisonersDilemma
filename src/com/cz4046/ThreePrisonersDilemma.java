0180c363b498599671a76443ab2763d1d91db454
Wilson <wilsonteng89@gmail.com>
2020-04-23 20:31:28 +0800

    Edited ThreePrisonersDilemma.java code
    This is to test for "good" agents more accurately.
    Runs the 100 rounds tournament a stipulated amount of times. Then observe which agent consistently places in the low ranks.
    
    Also added agents from the following github links to pit my agent against:
    1) https://github.com/wayneczw/cz4046/blob/master/assignment2/Main/Chen_Zhiwei_Player.java
    2) https://github.com/almightyGOSU/CZ4046-Intelligent-Agents-Assignment-2/blob/master/src/ThreePrisonersDilemma.java

--- src/com/cz4046/ThreePrisonersDilemma.java
+++ src/com/cz4046/ThreePrisonersDilemma.java
@@ -1,7 +1,12 @@
 package com.cz4046;
 
+import java.util.*;
+
 public class ThreePrisonersDilemma {
-    boolean VERBOSE = true;
+    static boolean VERBOSE = false;
+    static int TOURNAMENT_ROUNDS = 300;
+    static int NUM_OF_PLAYERS = 11;
+
 	/*
 	 This Java program models the two-player Prisoner's Dilemma game.
 	 We use the integer "0" to represent cooperation, and "1" to represent
@@ -116,6 +121,250 @@ public class ThreePrisonersDilemma {
         }
     }
 
+    /* Other Strategies */
+    /* From https://github.com/almightyGOSU/CZ4046-Intelligent-Agents-Assignment-2/blob/master/src/ThreePrisonersDilemma.java*/
+    class SoreLoser extends Player {
+
+        int myScore = 0;
+        int opp1Score = 0;
+        int opp2Score = 0;
+
+        int selectAction(int n, int[] myHistory, int[] oppHistory1, int[] oppHistory2) {
+
+            if (n == 0) {
+                return 0; // cooperate by default
+            }
+
+            // get the recent history index
+            int i = n - 1;
+
+            // add up the total score/points for each player
+            myScore += payoff[myHistory[i]][oppHistory1[i]][oppHistory2[i]];
+            opp1Score += payoff[oppHistory1[i]][oppHistory2[i]][myHistory[i]];
+            opp2Score += payoff[oppHistory2[i]][myHistory[i]][oppHistory1[i]];
+
+            // if my score is lower than the any of them
+            // it means that at least one of them have defected
+            if (myScore >= opp1Score && myScore >= opp2Score) {
+                // cooperate if my score is higher or equal than all of them
+                return 0;
+            }
+            return 1; // defect if my score is lower than any of them
+        }
+    }
+
+    /* Nasty Player From https://github.com/almightyGOSU/CZ4046-Intelligent-Agents-Assignment-2/blob/master/src/ThreePrisonersDilemma.java*/
+    class Nasty2 extends NastyPlayer {
+
+        //Count the number of defects by opp
+        int intPlayer1Defects = 0;
+        int intPlayer2Defects = 0;
+
+        //Store the round where agent retaliate against defects
+        int intRoundRetailate = -1;
+
+        //Number of rounds where agent coop to observer opp actions
+        int intObservationRound = 1;
+
+        //Number of rounds where agent retaliate defects with defects
+        //After this round, see opp actions to check if they decide to coop again
+        int intGrudgeRound = 3;
+
+        int selectAction(int n, int[] myHistory, int[] oppHistory1, int[] oppHistory2) {
+
+            //Record Defects count
+            if (n > 0) {
+                intPlayer1Defects += oppHistory1[n - 1];
+                intPlayer2Defects += oppHistory2[n - 1];
+            }
+
+            //Start by cooperating
+            if (n < intObservationRound) {
+                return 0; //cooperate by default
+            }
+
+            //Loop rounds where agent coop to reverse the effects of retaliation
+            if (intRoundRetailate < -1) {
+                intRoundRetailate += 1;
+                intPlayer1Defects = 0;
+                intPlayer2Defects = 0;
+                return 0;
+            }
+
+            //Check at round retaliated + threshold to measure if opp wishes to coop again
+            if (intRoundRetailate > -1 && n == intRoundRetailate + intGrudgeRound + 1) {
+                //Count the number of coop during retaliate round to check opp coop level
+                int intPlayer1Coop = 0;
+                int intPlayer2Coop = 0;
+
+                for (int intCount = 0; intCount < intGrudgeRound; intCount++) {
+                    intPlayer1Coop += oppHistory1[n - 1 - intCount] == 0 ? 1 : 0;
+                    intPlayer2Coop += oppHistory2[n - 1 - intCount] == 0 ? 1 : 0;
+                    //intPlayer1Coop += oppHistory1[n - 1 - intCount] == 1 ? 1 : 0;
+                    //intPlayer2Coop += oppHistory2[n - 1 - intCount] == 1 ? 1 : 0;
+                }
+
+                //If both players wish to coop again, start to coop with them
+                if (intPlayer1Coop > 1 && intPlayer2Coop > 1 && (oppHistory1[n - 1] + oppHistory2[n - 1]) == 0) {
+                    //Hold round where agent coop to show intention to coop again
+                    //Count backwards from -2
+                    //-2 indicates 1 round where agent coop to reverse effect of retailation
+                    //-5 indicates 4 rounds where agent coop to reverse effect
+                    intRoundRetailate = -2;
+                    intPlayer1Defects = 0;
+                    intPlayer2Defects = 0;
+                    return 0;
+                } else {
+                    intRoundRetailate = n;
+                    return 1;
+                }
+
+            }
+
+            //Punish Defection by defecting straight away
+            //Stores the round defected
+            if (intPlayer1Defects + intPlayer2Defects > 0) {
+                intRoundRetailate = n;
+                return 1;
+            }
+            //Coop as default action
+            return 0;
+        }
+    }
+
+    /* Nice Player From https://github.com/almightyGOSU/CZ4046-Intelligent-Agents-Assignment-2/blob/master/src/ThreePrisonersDilemma.java*/
+    class Nice2 extends NicePlayer {
+
+        // For tracking Defect/Cooperate probabilities
+        private double opp1Def = 0;
+        private double opp2Def = 0;
+
+        // Thresholds
+        private static final double FRIENDLY_THRESHOLD = 0.850;
+        private static final double DEFENSIVE_THRESHOLD = 0.750;
+
+        int selectAction(int n, int[] myHistory, int[] oppHistory1, int[] oppHistory2) {
+
+            // Start by cooperating
+            if (n == 0) {
+
+                return 0;
+            }
+
+            // Calculate probability for Def/Coop (Opponent 1)
+            opp1Def += oppHistory1[n - 1];
+            double opp1DefProb = opp1Def / oppHistory1.length;
+            double opp1CoopProb = 1.000 - opp1DefProb;
+
+            // Calculate probability for Def/Coop (Opponent 2)
+            opp2Def += oppHistory2[n - 1];
+            double opp2DefProb = opp2Def / oppHistory2.length;
+            double opp2CoopProb = 1.000 - opp2DefProb;
+
+            /*System.out.printf("Opponent 1: %.3f, %.3f, Opponent 2: %.3f, %.3f%n",
+					opp1CoopProb, opp1DefProb, opp2CoopProb, opp2DefProb);*/
+            if (opp1CoopProb >= FRIENDLY_THRESHOLD
+                    && opp2CoopProb >= FRIENDLY_THRESHOLD
+                    && oppHistory1[n - 1] == 0
+                    && oppHistory2[n - 1] == 0) {
+
+                // Good chance that both opponents will cooperate
+                // Just cooperate so that everyone will be happy
+                return 0;
+
+            } else if ((opp1DefProb >= DEFENSIVE_THRESHOLD || opp2DefProb >= DEFENSIVE_THRESHOLD)
+                    && (oppHistory1[n - 1] == 1 || oppHistory2[n - 1] == 1)) {
+
+                // Given that one of the opponents have been relatively nasty,
+                // and one of them has defected in the previous turn,
+                // high prob that one of them will defect again,
+                // defect to protect myself!
+                return 1;
+
+            } else if (n >= 2) {
+
+                // Check if either opponent has defected in the last 2 turns
+                if (oppHistory1[n - 1] == 1 || oppHistory2[n - 1] == 1
+                        || oppHistory1[n - 2] == 1 || oppHistory2[n - 2] == 1) {
+
+                    // DESTROY them!!
+                    return 1;
+                } else {
+
+                    // Just be friendly!
+                    return 0;
+                }
+            } else {
+
+                // At this moment, both players are not that friendly,
+                // and yet neither of them are relatively nasty.
+                // Just be friendly for now.
+                return 0;
+            }
+        }
+    }
+
+    /* From https://github.com/wayneczw/cz4046/blob/master/assignment2/Main/ThreePrisonersDilemma.java */
+    class EncourageCoop extends Player {
+        int myScore = 0;
+        int opp1Score = 0;
+        int opp2Score = 0;
+
+        int selectAction(int n, int[] myHistory, int[] oppHistory1, int[] oppHistory2) {
+            // First Law: Always cooperate in first 2 rounds
+            if (n < 2) return 0;
+
+            // Second Law: Tolerate 2 consecutive defects from both opp
+            // If 2 consecutive defects from both opp, then defect
+            if (oppHistory1[n-1] == 1 && oppHistory1[n-2] == 1 &&
+                    oppHistory2[n-1] == 1 && oppHistory2[n-2] == 1)
+                return 1;
+
+            // Third Law: if one of the opponents is Nasty, then always defect
+            boolean isOpp1Nasty, isOpp2Nasty;
+            isOpp1Nasty = isNasty(n, oppHistory1);
+            isOpp2Nasty = isNasty(n, oppHistory2);
+            if (isOpp1Nasty || isOpp2Nasty) return 1;
+
+            // Fourth Law: if one of the opponents is Random, then always defect
+            boolean isOpp1Random, isOpp2Random;
+            isOpp1Random = isRandom(n, oppHistory1);
+            isOpp2Random = isRandom(n, oppHistory2);
+            if (isOpp1Random || isOpp2Random) return 1;
+
+            // Fifth Law: if my current score is lower than one of the opp, then always defect
+            myScore += payoff[myHistory[n-1]][oppHistory1[n-1]][oppHistory2[n-1]];
+            opp1Score += payoff[oppHistory1[n-1]][oppHistory2[n-1]][myHistory[n-1]];
+            opp2Score += payoff[oppHistory2[n-1]][oppHistory1[n-1]][myHistory[n-1]];
+            if (myScore < opp1Score || myScore < opp2Score) return 1;
+
+            // Sixth Law: If above laws don't apply, then be a T4TPlayer
+            if (Math.random() < 0.5) return oppHistory1[n-1];
+            else return oppHistory2[n-1];
+        }
+
+        boolean isNasty(int n, int[] oppHistory) {
+            int cnt = 0;
+            for (int i=0; i<n; i++){
+                if (oppHistory[i] == 1)
+                    cnt++;
+            }
+            if (cnt == n) return true;
+            else return false;
+        }
+
+        boolean isRandom(int n, int[] oppHistory) {
+            int sum = 0;
+            double eps = 0.025;
+            for (int i=0; i<n; i++) {
+                sum += oppHistory[i];
+            }
+            // if ratio is roughly 0.5, then the opponent is highly likely to be random
+            double ratio = (double) sum / n;
+            if (Math.abs(ratio - 0.5) < eps) return true;
+            else return false;
+        }
+    }
 
     /* In our tournament, each pair of strategies will play one match against each other.
      This procedure simulates a single match and returns the scores. */
@@ -152,7 +401,7 @@ public class ThreePrisonersDilemma {
 	 (strategies) in between matches. When you add your own strategy,
 	 you will need to add a new entry to makePlayer, and change numPlayers.*/
 
-    int numPlayers = 7;
+    int numPlayers = NUM_OF_PLAYERS;
     Player makePlayer(int which) {
         switch (which) {
             case 0: return new WILSON_TENG_Player();
@@ -162,6 +411,10 @@ public class ThreePrisonersDilemma {
             case 4: return new TolerantPlayer();
             case 5: return new FreakyPlayer();
             case 6: return new T4TPlayer();
+            case 7: return new SoreLoser();
+            case 8: return new Nasty2();
+            case 9: return new Nice2();
+            case 10: return new EncourageCoop();
         }
         throw new RuntimeException("Bad argument passed to makePlayer");
     }
@@ -169,13 +422,50 @@ public class ThreePrisonersDilemma {
     /* Finally, the remaining code actually runs the tournament. */
 
     public static void main (String[] args) {
+        int tournament_rounds = TOURNAMENT_ROUNDS;
+        int num_of_players = NUM_OF_PLAYERS;
+        int val;
+
         ThreePrisonersDilemma instance = new ThreePrisonersDilemma();
-        instance.runTournament();
+        LinkedHashMap<Integer, Integer> hashMap = new LinkedHashMap<>();
+        for (int player=0; player<num_of_players; player++)
+            hashMap.put(player, 0);
+
+        for (int i=0; i<tournament_rounds; i++) {
+            int[] top_players = instance.runTournament();
+            for (int tp=0; tp<3; tp++) {
+                System.out.println(top_players[tp]);
+            }
+            for (int p=0; p<top_players.length; p++) {
+                int tp = top_players[p];
+                val = hashMap.get(tp);
+                hashMap.put(tp, val+p+1);
+            }
+        }
+
+//        for (Map.Entry<Integer, Integer> entry : hashMap.entrySet()) {
+//            System.out.println(entry.getKey() + " = " + entry.getValue());
+//        }
+        hashMap = (LinkedHashMap<Integer, Integer>) sortByValue(hashMap);
+
+        float float_tournament_rounds = (float) tournament_rounds;
+        float float_val;
+        LinkedHashMap<Integer, Float> newHashMap = new LinkedHashMap<>();
+        for (int p=0; p<num_of_players; p++) {
+            val = hashMap.get(p);
+            float_val = (float) val;
+            newHashMap.put(p, float_val/float_tournament_rounds);
+        }
+
+        hashMap = (LinkedHashMap<Integer, Integer>) sortByValue(hashMap);
+        newHashMap = (LinkedHashMap<Integer, Float>) sortByValue(newHashMap);
+        System.out.println(hashMap);
+        System.out.println(newHashMap);
     }
 
     boolean verbose = VERBOSE; // set verbose = false if you get too much text output
 
-    void runTournament() {
+    int[] runTournament() {
         float[] totalScore = new float[numPlayers];
 
         // This loop plays each triple of players against each other.
@@ -219,9 +509,35 @@ public class ThreePrisonersDilemma {
         if (verbose) System.out.println();
         System.out.println("Tournament Results");
         for (int i=0; i<numPlayers; i++)
-            System.out.println(makePlayer(sortedOrder[i]).name() + ": "
+            System.out.println("[" + sortedOrder[i] + "] " + makePlayer(sortedOrder[i]).name() + ": "
                     + totalScore[sortedOrder[i]]/count + " points.");
+
+//        int[] top_players = new int[3];
+//        for (int i=0; i<3; i++) {
+//            top_players[i] = sortedOrder[i];
+//        }
+
+//        System.out.println();
+//        for (int i=0; i<3; i++) {
+//            System.out.println(top_players[i]);
+//        }
+        System.out.println();
+        return sortedOrder;
     } // end of runTournament()
+
+    public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map) {
+        List<Map.Entry<K, V>> list = new ArrayList<>(map.entrySet());
+        list.sort(Map.Entry.comparingByValue());
+//        Collections.reverse(list);
+
+        Map<K, V> result = new LinkedHashMap<>();
+        for (Map.Entry<K, V> entry : list) {
+            result.put(entry.getKey(), entry.getValue());
+        }
+
+        return result;
+    }
+
 } // end of class PrisonersDilemma
 
 
